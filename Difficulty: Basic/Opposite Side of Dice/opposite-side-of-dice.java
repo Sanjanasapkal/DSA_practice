4d26a3a6861adb50cb7d57da159639cc8d713ddf
Sanjanasapkal <sanjanaspkl@gmail.com>
2026-08-15 22:16:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Opposite Side of Dice/opposite-side-of-dice.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int oppositeFaceOfDice(int n) {
+        // code here
+        switch(n)
+        {
+            case 1:return 6;
+            case 2:return 5;
+            case 3:return 4;
+            case 4:return 3;
+            case 5:return 2;
+            case 6:return 1;
+        }
+        return 0;
+    }
+}
